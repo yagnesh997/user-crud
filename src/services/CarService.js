838660a69fd7489085d21779b58ecb5c4e9b8566
yagnesh jogani <yagneshjogani997@gmail.com>
2022-12-27 16:14:41 +0530

    add jsdoc file

--- src/services/CarService.js
+++ src/services/CarService.js
@@ -1,27 +1,47 @@
 
 const Car = require('../models/CarModels')
 
+/**
+ * Create a car
+ * @param {Object} car
+ * @returns {Promise<Car>}
+ */
 const create = async (car) => {
     return Car.create(car);
   };
   
-  
+/**
+ * Find all car
+ * @returns {Promise<Car[]>}
+ */
   const findAll = async () => {
     
     return await Car.find().exec();
   };
   
-  
+  /**
+   * Find a car by id
+   * @param {object} id 
+   * @returns {Promise<Car>}
+   */
   const findOne = async (id) => {
     return await Car.findOne({ _id: id }).exec();
   };
   
-  
+  /**
+   * Update a car by id
+   * @param {object} id 
+   * @returns {Promise<Car>}
+   */
   const update = async (id, car) => {
     return await Car.findOneAndUpdate({ _id: id }, car, { new: true });
   };
   
-  
+  /**
+   * Remove a car by id
+   * @param {object} id 
+   * @returns {Promise<Car>}
+   */
   const remove = async (id) => {
     return await Car.findOneAndDelete({ _id: id });
   };
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,27 +1,47 @@
 
 const User = require('../models/UserModels')
 
+/**
+ * Create a user
+ * @param {Object} user
+ * @returns {Promise<User>}
+ */
 const create = async (user) => {
     return User.create(user);
   };
   
-  
+/**
+ * Find all user
+ * @returns {Promise<User[]>}
+ */
   const findAll = async () => {
     
     return await User.find().exec();
   };
   
-  
+  /**
+   * Find a user by id
+   * @param {object} id 
+   * @returns {Promise<User>}
+   */
   const findOne = async (id) => {
     return await User.findOne({ _id: id }).exec();
   };
   
-  
+  /**
+   * Update a user by id
+   * @param {object} id 
+   * @returns {Promise<User>}
+   */
   const update = async (id, user) => {
     return await User.findOneAndUpdate({ _id: id }, user, { new: true });
   };
   
-  
+  /**
+   * Remove a user by id
+   * @param {object} id 
+   * @returns {Promise<User>}
+   */
   const remove = async (id) => {
     return await User.findOneAndDelete({ _id: id });
   };
